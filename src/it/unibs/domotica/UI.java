c903d644925648145ae5e4a7ec9481d91d8507c6
Federico <59961650+PolenTz@users.noreply.github.com>
2020-01-22 17:15:05 +0100

    Implementazione autenticazione interfaccia grafica

--- src/it/unibs/domotica/UI.java
+++ src/it/unibs/domotica/UI.java
@@ -34,20 +34,24 @@ public class UI {
                 "1.Manutentore\n" +
                 "2.Fruitore\n");
         int selezione = input.nextInt();
-        if(selezione == 1)
-        {
+
             System.out.println("Benvenuto! " +
                     "Inserire username:");
             String username = input.nextLine();
-            if(username.equals(this.manutentore.getUsername()))
+            if(username.equals(this.manutentore.getUsername()) && selezione == 1 || username.equals(this.fruitore.getUsername()) && selezione == 0)
             {
                 System.out.println("Inserire password:");
                 String password = input.nextLine();
-                if(password.equals(this.manutentore.getPassword()))
+                if(password.equals(this.manutentore.getPassword()) && selezione == 1)
+                {
+                    return 1;
+                }
+                else if(password.equals(this.fruitore.getPassword()) && selezione == 0)
                 {
                     return 0;
                 }
+
             }
-        }
+        return -1;
     }
 }
