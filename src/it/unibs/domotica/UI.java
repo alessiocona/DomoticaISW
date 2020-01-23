7066ca6c504875d82c4971f0f837d7c14ba4ac3c
Federico <59961650+PolenTz@users.noreply.github.com>
2020-01-23 17:22:59 +0100

    Implementazione completa della modalita' fruitore, inizio modalita' manutentore (Tutto da testare)

--- src/it/unibs/domotica/UI.java
+++ src/it/unibs/domotica/UI.java
@@ -21,96 +21,139 @@ public class UI {
         this.input = new Scanner(System.in);
     }
 
-    private void inizializazione()
+    private void menu()
     {
         //Necessita' di acquisire da disco le categorie pre-salvate
+        int selezione = 0;
         UI grafica = new UI(new Utente(), new Utente(), categorie, new UnitaImmobiliare());
+        //Se 1 loggato manutentore, se 2 loggato fruitore, se 0 esci dal programma
+        System.out.println("Benvenuto nel sistema domotico.\n");
 
-        //Se 1 loggato manutentore, se 2 loggato fruitore, se -1 esci dal programma
-        int modalitaUtente = grafica.autenticazione();
-        if (modalitaUtente == 1)
-        {
-            grafica.modalitaManutentore();
-        }
-        else if (modalitaUtente == 2)
-        {
-            grafica.modlitaFruitore();
-        }
-        return;
-
-
-    }
-
-    private int autenticazione()
-    {
         do {
-            System.out.println("Benvenuto nel sistema domotico.\n" +
-                    "Selezionare la modalita' di utilizzo:\n\n" +
+            System.out.println("Selezionare la modalita' di utilizzo:\n\n" +
                     "1. Manutentore\n" +
                     "2. Fruitore\n" +
                     "0. Esci\n");
-            int selezione = input.nextInt();
-            if (selezione == 0)
-                return 0;
-            System.out.println("Benvenuto! " +
-                    "Inserire username:");
-            String username = input.nextLine();
-            if(username.equals(this.manutentore.getUsername()) && selezione == 1 || username.equals(this.fruitore.getUsername()) && selezione == 0)
+            try
             {
-                System.out.println("Inserire password:");
-                String password = input.nextLine();
-                if(password.equals(this.manutentore.getPassword()) && selezione == 1)
-                {
-                    return 1;
-                }
-                else if(password.equals(this.fruitore.getPassword()) && selezione == 0)
-                {
-                    return 2;
-                }
-
+                selezione = input.nextInt();
+            }catch(Exception e)
+            {
+                System.out.print("\nParamentro non consentito, reiserire valore: ");
+                continue;
             }
-            System.out.println("Username o password errati!! \n\n");
-        }while(true);
+            switch (selezione) {
+                case 1:
+                    if (this.autenticazione(this.manutentore))
+                    {
+                        System.out.println("Selezionare l'operazione:\n\n" +
+                                "1. Aggiungi categoria di sensori/attuatori\n" +
+                                "2. Aggiungi unita' immobiliare\n" +
+                                "3. Aggiungi sensore/attuatore ad una categoria\n" +
+                                "4. Creare unita' abitativa\n" +
+                                "5. Selezionare unita' abitativa\n" +
+                                "0. Esci\n");
+                        do {
 
+                        }while(true);
+                    }
+                    break;
+                case 2:
+                    if (this.autenticazione(this.fruitore))
+                    {
+                        this.visualizzaUnitaImmobiliare();
+                        System.out.print("Premere 1 per visualizzare tutti i sensori dell'unita' immobiliare: ");
+                        do {
+                            try{
+                                selezione = input.nextInt();
+                                if (selezione != 1)
+                                    System.out.print("\nParamentro non consentito, reiserire valore: ");
+                            }catch(Exception e)
+                            {
+                                System.out.print("\nParamentro non consentito, reiserire valore: ");
+                            }
+                        }while(selezione != 1);
+                        this.visualizzaListaSensori();
+                        do {
+                            System.out.println("Premere 1 per aggiornare i valori, premere 0 per tornare al menu principale");
+                            try{
+                                selezione = input.nextInt();
+                                if (selezione != 1 || selezione != 0)
+                                    System.out.print("\nParamentro non consentito, reiserire valore: ");
+                                else if (selezione == 1)
+                                {
+                                    this.visualizzaListaSensori();
+                                }
+                            }catch(Exception e)
+                            {
+                                System.out.print("\nParamentro non consentito, reiserire valore: ");
+                            }
+                        }while(selezione != 0);
+                    }
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.print("Input non valido, inserire nuovo valore: ");
+                    break;
+            }
+        }while(selezione != 0);
     }
 
-    private void modalitaManutentore()
+    private boolean autenticazione(Utente user)
     {
-
+        System.out.print("Inserire username: ");
+        String username = input.nextLine();
+        if(username.equals(user.getUsername()))
+        {
+            System.out.print("Inserire password: ");
+            String password = input.nextLine();
+            if(password.equals(user.getPassword()))
+            {
+                return true;
+            }
+        }
+        return false;
     }
-    private int modlitaFruitore()
+
+    private void visualizzaUnitaImmobiliare()
     {
         //Prelevo solamente l'unica unita' immobiliare disponibile, cioe' solo la prima
+
         System.out.println("Questa e' la sua unita' immobiliare: \n" +
                 "-" + this.fruitore.getUnitaImmobiliari().get(0).getNome() + "\n" +
                 "-" + this.fruitore.getUnitaImmobiliari().get(0).getTipologia() + "\n" +
                 "-" + this.fruitore.getUnitaImmobiliari().get(0).getDescrizione() + "\n");
+    }
 
-        int i = 1;
-        System.out.println("Lista dei sensori visualizzabili, 0 per tornare indietro:");
+    private void visualizzaListaSensori()
+    {
+        System.out.println("Lista dei sensori: ");
+        System.out.print("Stanze: \n");
+        //Lista dei sensori delle stenza, e dei relativi artefatti all'interno delle stanze
         for (Stanza s : this.fruitore.getUnitaImmobiliari().get(0).getStanze()) {
-            System.out.println("-" + s.getNome() + "\n");
-            for (Sensore sStanza : s.getSensori())
+            System.out.print(s.getNome() + "\t");
+            for (Sensore sensoreStanza : s.getSensori())
             {
-                System.out.print(i + ". " + sStanza.getNome() + "\n");
-                i++;
+                System.out.print("\n" +sensoreStanza.getNome() + " " + sensoreStanza.getValore() + " " + sensoreStanza.getUnitaDiMisura());
             }
             for (Artefatto a : s.getArtefatti())
             {
-                for(Sensore sArtefatto : a.getSensori())
+                for(Sensore sensoreArtefatto : a.getSensori())
                 {
-                    System.out.print(i + ". " + sArtefatto.getNome() + "\n");
-                    i++;
+                    System.out.print("\n" + sensoreArtefatto.getNome() + " " + sensoreArtefatto.getValore() + " " + sensoreArtefatto.getUnitaDiMisura());
                 }
+            }
 
+        }
+        System.out.print("Artefatti esterni: "); //Lista dei sensori degli artefatti esterni alla casa
+        for(Artefatto arte : this.fruitore.getUnitaImmobiliari().get(0).getEsterno())
+        {
+            for(Sensore s : arte.getSensori())
+            {
+                System.out.print("\n" + s.getNome() + " " + s.getValore() + " " + s.getUnitaDiMisura());
             }
         }
-        int selezione = input.nextInt();
-        if (selezione == 0)
-        {
-            return 0;
-        }
-
-        return 1;
     }
+
 }
